b48104dcf3edf6ec4ac9a39baee810c71c71ecd2
Harivansh-coder <harivanshthakur8@gmail.com>
2025-05-17 20:36:54 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { envVariables } from "./utils/env";
+
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(helmet());
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+export default app;
+
+// start server for development environment
+if (envVariables.NODE_ENV === "development") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
